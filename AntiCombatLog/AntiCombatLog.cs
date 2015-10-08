69ac8f66dc829e4bc91d6de178a659874015c7cb
False_Chicken <jmdevsupport@gmail.com>
2015-10-08 01:15:20 -0400

    Init commit.

---
+++ AntiCombatLog/AntiCombatLog.cs
@@ -0,0 +1,52 @@
+//
+//  AntiCombatLog.cs
+//
+//  Author: False_Chicken
+//  Contact: jmdevsupport@gmail.com
+//
+//  Copyright (c) 2015 False_Chicken
+//
+//  This program is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation; either version 2 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+//
+using System;
+using Rocket.Core.Plugins;
+using Rocket.API;
+
+namespace FC.AntiCombatLog
+{
+	public class AntiCombatLog : RocketPlugin<AntiCombatLogConfiguration>
+	{
+		#region CONSTANTS
+
+		private const string C_VERSION = "0.1";
+
+		#endregion
+
+		public AntiCombatLog ()
+		{
+		}
+
+		protected override void Load()
+		{
+
+		}
+
+		void FixedUpdate()
+		{
+
+		}
+	}
+}
+
---
+++ README.txt
@@ -0,0 +1,3 @@
+-- AntiCombatLog --
+
+A Rocket plug-in that punishes players who disconnect from the server after a configurable number of seconds after taking damage.
